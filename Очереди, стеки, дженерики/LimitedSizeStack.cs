7bbdf001883280c96ae3a44b783a6dac528acd88
Eized <brookenlp1@gmail.com>
2021-02-27 13:49:56 +0500

    .
    .

---
+++ Очереди, стеки, дженерики/LimitedSizeStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApplication
+{
+    public class LimitedSizeStack<T>
+    {
+        public Dictionary<int,T> Stack;
+        public int Limit;
+        public int Head;
+        public int Tail;
+        public LimitedSizeStack(int limit)
+        {
+            Stack = new Dictionary<int, T>();
+            Limit = limit;
+            Head = 0;
+            Tail = 0;
+        }
+
+        public void Push (T item)
+        {
+            if (Limit == 0) return;
+            if(Stack.Count() == Limit)
+            {
+                Stack.Remove(Tail);
+                Tail++;
+            }
+            Stack.Add(Head, item);
+            Head++;
+        }
+
+        public T Pop()
+        {
+            var output = Stack[Head- 1];
+            Stack.Remove(Head - 1);
+            Head--;
+            return output;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return Stack.Count();
+            }
+        }
+    }
+}
---
+++ Очереди, стеки, дженерики/ListModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApplication
+{
+    public class ListModel<TItem>
+    {
+        public List<TItem> Items { get; }
+        public int Limit;
+        public LimitedSizeStack<UserAction<TItem>> Stack;
+
+        public ListModel(int limit)
+        {
+            Items = new List<TItem>();
+            Limit = limit;
+            Stack = new LimitedSizeStack<UserAction<TItem>>(limit);
+        }
+
+        public void AddItem(TItem item)
+        {
+            var action = new UserAction<TItem>("add", Items.Count, item);
+            Stack.Push(action);
+            Items.Add(item);
+        }
+
+        public void RemoveItem(int index)
+        {
+            if (Items.Count == 0) return;
+            var action = new UserAction<TItem>("remove", index, Items[index]);
+            Stack.Push(action);
+            Items.RemoveAt(index);
+        }
+
+        public bool CanUndo()
+        {
+            var size = Stack.Count;
+            return size != 0 && size < Limit;
+        }
+
+        public void Undo()
+        {
+            if (CanUndo())
+            {
+                var action = Stack.Pop();
+                if (action.Event == "remove")
+                    Items.Insert(action.Index, action.Value);
+                else if(action.Event == "add")
+                    Items.RemoveAt(action.Index);
+            }
+        }
+    }
+
+    public class UserAction<TItem>
+    {
+        public string Event;
+        public int Index;
+        public TItem Value;
+        public UserAction(string action, int index, TItem value)
+        {
+            Event = action;
+            Value = value;
+            Index = index;
+        }
+    }
+}
